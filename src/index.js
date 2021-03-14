c0ef9dd53c260705a364f7f0cd6b0f8f34aafcf6
Vitali Herasimenka <vitttold@inbox.ru>
2021-03-14 13:33:02 +0300

    add: solition

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    let res = [];
+
+    if (Array.isArray(matrix)) {
+        for (let i = 1; i < matrix.length; i = i + 2) {
+            matrix[i].reverse();
+        }
+        for (let k = 0; k < matrix.length; k++) {
+            res = res.concat(matrix[k]);
+        }
+    }
+    return res;
+};
